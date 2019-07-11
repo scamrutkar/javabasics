eb0f3ae3dcfdd1a8c4c2e6dae529b08c8b3749fa
scamrutkar <scamrutkar@gmail.com>
2019-07-11 11:02:31 +0530

    adding practice file

--- JavaBasics/src/com/sagar/javacase/Practise.java
+++ JavaBasics/src/com/sagar/javacase/Practise.java
@@ -11,12 +11,28 @@ class Test {
 		Test obj = new Test();;
 		System.out.println(obj.foo());
 	}
+	
+	public void add(int a, double b) {
+		System.out.println("Add first int called");
+		System.out.println(a+b);
+	}
+	
+	public void add(double a, int b) {
+		System.out.println("Add first double called");
+		System.out.println(a+b);
+	}
 }
 
 
 public class Practise {
 	
 	public static void main(String[] args) {
+		
+		/*
+		 * Ambiguity exception while calling add method 
+		 * Test test = new Test();
+		 * test.add(12,5);
+		 */
 		
 		String a = "Test";
 		String b = new String("Test");
