a67cb040a6c3d693c85201ad80b62b4657d2c915
scamrutkar <scamrutkar@gmail.com>
2019-07-12 14:24:48 +0530

    Generics

--- JavaBasics/src/com/sagar/javacase/GenericsExample.java
+++ JavaBasics/src/com/sagar/javacase/GenericsExample.java
@@ -18,11 +18,11 @@ public class GenericsExample {
 	
 	public static void main(String[] args) {
 		
-		GenericTypes<String> stringObj = new GenericTypes();
+		GenericTypes<String> stringObj = new GenericTypes<String>();
 		stringObj.setT("String Object");
 		System.out.println(stringObj.toString());
 		
-		GenericTypes<String> intObj = new GenericTypes();
+		GenericTypes<String> intObj = new GenericTypes<String>();
 		stringObj.setT("Integer Object");
 		System.out.println(intObj.toString());
 		
