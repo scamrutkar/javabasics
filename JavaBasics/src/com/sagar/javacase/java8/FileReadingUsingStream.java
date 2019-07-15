6961dcafe4bff4acbc8d62fa09fa6c7bee2eb665
scamrutkar <scamrutkar@gmail.com>
2019-07-15 09:16:03 +0530

    adding java8 features implementation

---
+++ JavaBasics/src/com/sagar/javacase/java8/FileReadingUsingStream.java
@@ -0,0 +1,35 @@
+package com.sagar.javacase.java8;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class FileReadingUsingStream {
+
+	public static void main(String[] args) throws IOException {
+		
+		String fileName = "E:\\SampleFiles\\Sample1.txt";
+		
+		try {
+			Stream<String> stream = Files.lines(Paths.get(fileName));
+			stream.forEach(System.out::println);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		String line;
+		while((line = br.readLine())!=null) {
+			System.out.println(line);
+		}
+		
+		
+	}
+
+}
