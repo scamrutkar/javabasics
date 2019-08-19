d8a4341666d1b25669128f357227e915a866d744
scamrutkar <scamrutkar@gmail.com>
2019-08-19 16:48:18 +0530

    Java8 Practise

---
+++ JavaBasics/src/com/sagar/javacase/java8/MobileNumber.java
@@ -0,0 +1,19 @@
+package com.sagar.javacase.java8;
+
+public class MobileNumber {
+	
+	private String number;
+
+    public MobileNumber(String number) {
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+}
