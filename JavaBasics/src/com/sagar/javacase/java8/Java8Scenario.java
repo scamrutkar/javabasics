c7bcfe3d428f872d24733d733585cf5787eec011
scamrutkar <scamrutkar@gmail.com>
2019-08-22 11:36:10 +0530

    Java8 Scenario

--- JavaBasics/src/com/sagar/javacase/java8/Java8Scenario.java
+++ JavaBasics/src/com/sagar/javacase/java8/Java8Scenario.java
@@ -1,8 +1,12 @@
 package com.sagar.javacase.java8;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Java8Scenario {
 
@@ -28,6 +32,7 @@ public class Java8Scenario {
 		students.stream()
 				.filter((student) -> student.getName().equals("Jayesh"))
 				.forEach((student) -> System.out.println(student.getName() +" "+student.getAddress().toString()));
+		System.out.println("------------------------------------------------------------------------");
 		
 		/*****************************************************
         Get student with matching address "1235"
@@ -35,6 +40,7 @@ public class Java8Scenario {
 		students.stream()
 				.filter((student)->student.getAddress().getZipcode().equals("1235"))
 				.forEach((student) -> System.out.println(student.getName()));
+		System.out.println("------------------------------------------------------------------------");
 		
 		/*****************************************************
         Get all student having mobile numbers 3333.
@@ -42,10 +48,70 @@ public class Java8Scenario {
 		students.stream()
 			.filter( student -> student.getMobileNumbers().stream().anyMatch(mobNo -> mobNo.getNumber().equals("3333")))
 			.forEach(student -> System.out.println(student.getName()));
+		System.out.println("------------------------------------------------------------------------");
 		
 		/*****************************************************
         Get all student having mobile number 1233 and 1234
         *****************************************************/
+		List<Student> list = students.stream()
+			.filter(student -> student.getMobileNumbers().stream().allMatch(mobNo -> mobNo.getNumber().equals("4444") || mobNo.getNumber().equals("3333")))
+			.collect(Collectors.toList());
+		
+		String str = list.stream().map(student -> student.getName()).collect(Collectors.joining(",", "[", "]"));
+	    System.out.println(str);
+	    System.out.println("------------------------------------------------------------------------");
+	    
+	    /*******************************************************
+        Create a List<Student> from the List<TempStudent>
+       *********************************************************/
+		TempStudent tmpStud1 = new TempStudent("Jayesh1", 201, new Address("12341"),
+				Arrays.asList(new MobileNumber("12331"), new MobileNumber("12341")));
+
+		TempStudent tmpStud2 = new TempStudent("Khyati1", 202, new Address("12351"),
+				Arrays.asList(new MobileNumber("11111"), new MobileNumber("33331"), new MobileNumber("12331")));
+
+		List<TempStudent> tmpStudents = Arrays.asList(tmpStud1, tmpStud2);
+		
+		List<Student> studentList = tmpStudents.stream()
+														.map(tmpStd -> new Student(tmpStd.name, tmpStd.age, tmpStd.address, tmpStd.mobileNumbers))
+														.collect(Collectors.toList());
+		
+		System.out.println(studentList);
+		System.out.println("------------------------------------------------------------------------");
+		
+		/**********************************************************
+        Convert List<Student> to List<String> of student name
+       ***********************************************************/
+		
+		List<String> studentNameList = students.stream().map(student -> student.getName()).collect(Collectors.toList());
+		System.out.println(studentNameList);
+		System.out.println("------------------------------------------------------------------------");
+		
+		/*****************************************************
+        Change the case of List<String>
+       *****************************************************/
+		List<String> upperCaseList = studentNameList.stream().map(String::toUpperCase).collect(Collectors.toList());
+		System.out.println(upperCaseList);
+		System.out.println("------------------------------------------------------------------------");
+		
+		/*****************************************************
+        Sort List<String>
+        *****************************************************/
+		List<String> sortedStringList = studentNameList.stream().sorted((s1,s2)->s1.compareTo(s2)).collect(Collectors.toList());
+		System.out.println(sortedStringList);
+		System.out.println("------------------------------------------------------------------------");
+		
+		/*****************************************************
+        Comparator Interface Java8
+        *****************************************************/
+		System.out.println(students);
+		Collections.sort(students, 
+					Comparator.comparing(Student::getName)
+								.thenComparing(Student::getAge)
+								.reversed());
+		System.out.println(students);
+		System.out.println("------------------------------------------------------------------------");
+		
 
 	}
 
