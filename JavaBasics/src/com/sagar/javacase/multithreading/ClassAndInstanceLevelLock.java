3f731855127ac14d657044d8077ca821b01a4d70
Sagar Amrutkar <scamrutkar@gmail.com>
2019-08-26 08:19:41 +0530

    Implementation for class and instance level lock

---
+++ JavaBasics/src/com/sagar/javacase/multithreading/ClassAndInstanceLevelLock.java
@@ -0,0 +1,85 @@
+package com.sagar.javacase.multithreading;
+
+class ClassAndInstanceLevelLock {
+
+	public static void main(String[] args) {
+		final ClassAndInstanceLevelLock threadDemo1 = new ClassAndInstanceLevelLock();
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				threadDemo1.getA();
+			}
+		}).start();
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				threadDemo1.getB();
+			}
+		}).start();
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				ClassAndInstanceLevelLock.getC();
+			}
+		}).start();
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				ClassAndInstanceLevelLock.getD();
+			}
+		}).start();
+
+	}
+
+	/*** INSTANCE METHOD ***/
+	public synchronized void getA() {
+		System.out.println("ThreadDemo.getA() :" + Thread.currentThread().getName() + " enetered");
+		try {
+			Thread.sleep(2000);
+			wait();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("ThreadDemo.getA() :" + Thread.currentThread().getName() + " leaving");
+	}
+
+	public synchronized void getB() {
+		System.out.println("ThreadDemo.getB() :" + Thread.currentThread().getName() + " enetered");
+		try {
+			Thread.sleep(2000);
+			notify();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("ThreadDemo.getB() :" + Thread.currentThread().getName() + " leaving");
+	}
+
+	/*** CLASS METHOD ***/
+	public static synchronized void getC() {
+		System.out.println("ThreadDemo.getC() :" + Thread.currentThread().getName() + " enetered");
+		try {
+			Thread.sleep(2000);
+			ClassAndInstanceLevelLock.class.wait();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("ThreadDemo.getC() :" + Thread.currentThread().getName() + " leaving");
+	}
+
+	public static synchronized void getD() {
+		System.out.println("ThreadDemo.getD() :" + Thread.currentThread().getName() + " enetered");
+		try {
+			Thread.sleep(2000);
+			ClassAndInstanceLevelLock.class.notify();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("ThreadDemo.getD() :" + Thread.currentThread().getName() + " leaving");
+	}
+
+}
+
