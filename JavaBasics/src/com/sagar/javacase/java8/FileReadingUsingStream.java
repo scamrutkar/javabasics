bbb0a4da1c60b5a18ae8500566de7bad0b367bb1
scamrutkar <scamrutkar@gmail.com>
2020-03-30 12:58:00 +0530

    Changes in java8 scenario classes

--- JavaBasics/src/com/sagar/javacase/java8/FileReadingUsingStream.java
+++ JavaBasics/src/com/sagar/javacase/java8/FileReadingUsingStream.java
@@ -4,32 +4,30 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class FileReadingUsingStream {
 
 	public static void main(String[] args) throws IOException {
-		
+
 		String fileName = "E:\\SampleFiles\\Sample1.txt";
-		
+
 		try {
 			Stream<String> stream = Files.lines(Paths.get(fileName));
 			stream.forEach(System.out::println);
+			stream.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
 		BufferedReader br = new BufferedReader(new FileReader(fileName));
 		String line;
-		while((line = br.readLine())!=null) {
+		while ((line = br.readLine()) != null) {
 			System.out.println(line);
 		}
-		
-		
+		br.close();
+
 	}
 
 }
